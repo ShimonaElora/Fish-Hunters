e900b158439c0cb9711d0e9b3d1a63dce678f24e
Shimona Elora <umysharma@gmail.com>
2017-09-09 23:39:01 +0530

    WaterScript add

---
+++ Assets/Scripts/WaterScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour {
+
+    LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        //lineRenderer.SetPosition(0, )
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
